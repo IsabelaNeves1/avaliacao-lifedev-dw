60caeb402aca308e0476278e60e44901d98d55de
IsabelaNeves1 <nevesisabela03@gmail.com>
2025-05-10 04:02:14 -0300

    run build

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+//
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
